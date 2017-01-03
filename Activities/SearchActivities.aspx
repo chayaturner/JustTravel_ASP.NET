240d6260bce9bf260855be741389bc01134bb214
chayaturner <dietaholic@gmail.com>
2017-01-03 11:09:18 -0500

    Updated Search with GridView

--- Activities/SearchActivities.aspx
+++ Activities/SearchActivities.aspx
@@ -10,14 +10,46 @@
 </asp:Content>
 <asp:Content ID="Content2" ContentPlaceHolderID="cpMainContent" Runat="Server">
     <p>Search Activities by name:</p>
+    <asp:SqlDataSource ID="SqlDataSource1" runat="server" ConnectionString="<%$ ConnectionStrings:JustTravel %>" DeleteCommand="DELETE FROM [Activity] WHERE [ActivityID] = @ActivityID" InsertCommand="INSERT INTO [Activity] ([ActivityName], [Description], [Rating], [DateCreated]) VALUES (@ActivityName, @Description, @Rating, @DateCreated)" SelectCommand="SELECT [ActivityName], [Description], [Rating], [DateCreated], [ActivityID] FROM [Activity] WHERE ([ActivityName] = @ActivityName)" UpdateCommand="UPDATE [Activity] SET [ActivityName] = @ActivityName, [Description] = @Description, [Rating] = @Rating, [DateCreated] = @DateCreated WHERE [ActivityID] = @ActivityID">
+        <DeleteParameters>
+            <asp:Parameter Name="ActivityID" Type="Int32" />
+        </DeleteParameters>
+        <InsertParameters>
+            <asp:Parameter Name="ActivityName" Type="String" />
+            <asp:Parameter Name="Description" Type="String" />
+            <asp:Parameter Name="Rating" Type="Int32" />
+            <asp:Parameter Name="DateCreated" Type="DateTime" />
+        </InsertParameters>
+        <SelectParameters>
+            <asp:ControlParameter ControlID="SearchTextBox" Name="ActivityName" PropertyName="Text" Type="String" />
+        </SelectParameters>
+        <UpdateParameters>
+            <asp:Parameter Name="ActivityName" Type="String" />
+            <asp:Parameter Name="Description" Type="String" />
+            <asp:Parameter Name="Rating" Type="Int32" />
+            <asp:Parameter Name="DateCreated" Type="DateTime" />
+            <asp:Parameter Name="ActivityID" Type="Int32" />
+        </UpdateParameters>
+    </asp:SqlDataSource>
     <asp:TextBox ID="SearchTextBox" runat="server"></asp:TextBox>
+    <br />
     <br />
     <asp:Button ID="SearchButton" runat="server" OnClick="SearchButton_Click" Text="Search for Activity" />
     <br />
-    <asp:Label ID="NameResultsLabel" runat="server"></asp:Label> <br />
-    <asp:Label ID="DescriptionResultsLabel" runat="server"></asp:Label><br />
-    <asp:Label ID="RatingResultsLabel" runat="server"></asp:Label><br />
-    <asp:Label ID="DateResultsLabel" runat="server"></asp:Label>
+    <br />
+    <asp:GridView ID="GridView1" runat="server" AutoGenerateColumns="False" DataKeyNames="ActivityID" DataSourceID="SqlDataSource1">
+        <Columns>
+            <asp:CommandField ShowDeleteButton="True" ShowEditButton="True" ShowSelectButton="True" />
+            <asp:BoundField DataField="ActivityID" HeaderText="Activity" InsertVisible="False" ReadOnly="True" SortExpression="ActivityID" />
+            <asp:BoundField DataField="ActivityName" HeaderText="Name" SortExpression="ActivityName" />
+            <asp:BoundField DataField="Description" HeaderText="Description" SortExpression="Description" />
+            <asp:BoundField DataField="Rating" HeaderText="Rating" SortExpression="Rating" />
+            <asp:BoundField DataField="DateCreated" HeaderText="Date Created" SortExpression="DateCreated" />
+        </Columns>
+    </asp:GridView>
+    <br />
+    <asp:Label ID="ResultsLabel" runat="server" Text=""></asp:Label>
+    <br />
     <br />
     </asp:Content>
 
--- Activities/SearchActivities.aspx.cs
+++ Activities/SearchActivities.aspx.cs
@@ -16,40 +16,38 @@ public partial class Activities_SearchActivities : System.Web.UI.Page
     }
     protected void SearchButton_Click(object sender, EventArgs e)
     {
-        SqlConnection con = new SqlConnection("Data Source=(LocalDB)\\MSSQLLocalDB;AttachDbFilename=|DataDirectory|\\JustTravel.mdf;Integrated Security=True");
-        con.Open();
-        String search = SearchTextBox.Text;
+        //SqlConnection con = new SqlConnection("Data Source=(LocalDB)\\MSSQLLocalDB;AttachDbFilename=|DataDirectory|\\JustTravel.mdf;Integrated Security=True");
+        //con.Open();
+        //String search = SearchTextBox.Text;
         
-        //Get activity's name:
-        String nameQuery = "SELECT ActivityName FROM Activity WHERE ActivityName = '" + search + "'";
-        SqlCommand getname = con.CreateCommand();
-        getname.CommandText = nameQuery;
-        String name = ((String)getname.ExecuteScalar());
+        ////Get activity's name:
+        //String nameQuery = "SELECT ActivityName FROM Activity WHERE ActivityName = '" + search + "'";
+        //SqlCommand getname = con.CreateCommand();
+        //getname.CommandText = nameQuery;
+        //String name = ((String)getname.ExecuteScalar());
 
-        //Get activity's desription:
-        String descQuery = "SELECT Description FROM Activity WHERE ActivityName = '" + search + "'";
-        SqlCommand getDesc = con.CreateCommand();
-        getDesc.CommandText = descQuery;
-        String description = ((String)getDesc.ExecuteScalar());
+        ////Get activity's desription:
+        //String descQuery = "SELECT Description FROM Activity WHERE ActivityName = '" + search + "'";
+        //SqlCommand getDesc = con.CreateCommand();
+        //getDesc.CommandText = descQuery;
+        //String description = ((String)getDesc.ExecuteScalar());
 
-        //Get activity's rating:
-        String ratingQuery = "SELECT Rating FROM Activity WHERE ActivityName = '" + search + "'";
-        SqlCommand getRating = con.CreateCommand();
-        getRating.CommandText = ratingQuery;
-        int rating = ((int)getRating.ExecuteScalar());
+        ////Get activity's rating:
+        //String ratingQuery = "SELECT Rating FROM Activity WHERE ActivityName = '" + search + "'";
+        //SqlCommand getRating = con.CreateCommand();
+        //getRating.CommandText = ratingQuery;
+        //int rating = ((int)getRating.ExecuteScalar());
 
-        //Get activity's date:
-        String dateQuery = "SELECT DateCreated FROM Activity WHERE ActivityName = '" + search + "'";
-        SqlCommand getDate = con.CreateCommand();
-        getDate.CommandText = dateQuery;
-        DateTime date = ((DateTime)getDate.ExecuteScalar());
+        ////Get activity's date:
+        //String dateQuery = "SELECT DateCreated FROM Activity WHERE ActivityName = '" + search + "'";
+        //SqlCommand getDate = con.CreateCommand();
+        //getDate.CommandText = dateQuery;
+        //DateTime date = ((DateTime)getDate.ExecuteScalar());
+        
+        //con.Close();
 
-        //display search results in labels
-        NameResultsLabel.Text = "Name: " + name;
-        DescriptionResultsLabel.Text = "Description: " + description;
-        RatingResultsLabel.Text = "Rating: " + rating;
-        DateResultsLabel.Text = "Date Created: " + date;
+        ResultsLabel.Text = "Search Complete";
 
-        con.Close();
+        
     }
 }
